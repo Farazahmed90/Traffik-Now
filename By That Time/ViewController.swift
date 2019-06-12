e5d7f5d548a80d0e4563070a4e7dc068d9bb1d75
Abdul Moid <abdulmoid@Abduls-Mac.local>
2019-06-12 18:55:22 +0500

    Initial Commit

---
+++ By That Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  By That Time
+//
+//  Created by Abdul Moid on 12/06/2019.
+//  Copyright © 2019 www.d-tech.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
